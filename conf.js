1f2caecb727dad7031fd66541344ef3a20dbd138
神之浩 <31089649+hao45e@users.noreply.github.com>
2020-10-25 15:28:29 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -85,7 +85,7 @@ jsproxy_config({
    * URL 自定义处理（设计中）
    */
   url_handler: {
-    'https://www.baidu.com/img/baidu_resultlogo@2.png': {
+    /*'https://www.baidu.com/img/baidu_resultlogo@2.png': {
       replace: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png'
     },
     'https://www.pornhub.com/': {
@@ -93,6 +93,6 @@ jsproxy_config({
     },
     'http://haha.com/': {
       content: 'Hello World'
-    },
+    },*/
   }
 })
